6f9b603688f59e28d1ebcd50ef47306f591a3983
ksey <17622530+MrKsey@users.noreply.github.com>
2022-06-24 09:26:03 +0300

    Update qbt_resume_torrents.sh

--- qbt_resume_torrents.sh
+++ qbt_resume_torrents.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+echo " "
+echo "============================================="
+echo " "
+echo "$(date): Resuming torrent downloads ..."
+
 qbt torrent resume ALL
 qbt torrent reannounce ALL
+
+echo " "
+echo "============================================="
+echo " "
