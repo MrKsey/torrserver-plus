f04c504d27010c03a8fb3477533d1d50cdc01135
ksey <17622530+MrKsey@users.noreply.github.com>
2022-06-24 23:47:05 +0300

    Update ps_exit.sh

--- ps_exit.sh
+++ ps_exit.sh
@@ -5,11 +5,12 @@ echo "=================================================="
 echo " "
 # Graceful shutdown container
 echo "$(date): Graceful shutdown container ..."
+pkill -15 cron
 pkill -2 -f "^"qbittorrent-nox
 pkill -15 -f "^"/TS/TorrServer
-pkill -15 tail
-pkill -15 cron
-pkill -15 bash
 sync
+sleep 3
+pkill -15 tail
+pkill -15 bash
 echo " "
 echo "=================================================="
